a31d606d30d63942617dacf5c554ad118f57c289
Faisal Md <faisalindex@gmail.com>
2023-07-05 00:39:26 +0600

    44

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const app = require('./src-back/routes');
 const bot = require('./src-back/bot');
+var packageJson = require('./package.json');
+
 
 
 const PORT = process.env.PORT || 3000;
@@ -7,5 +9,5 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, async () => {
 //
-  console.log(`App running: | Port: ${PORT} | Env: ${process.env.NODE_ENV} ` );
+  console.log(`App running: | Port: ${PORT} | Env: ${process.env.NODE_ENV} | Version: ${packageJson.version}` );
 });
