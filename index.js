4f1c8cd325b8e32ec13b8413e89827980920624f
weblight <weblight@localhost.localdomain>
2023-07-04 20:37:51 +0600

    f: import webhookCallback #3

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
+const app = require('./src-back/routes');
 const { webhookCallback } = require("grammy");
 const express = require('express');
 const bot = require('./src-back/bot');
-const app = require('./src-back/routes');
 
 
 
@@ -12,17 +12,16 @@ const PORT = process.env.PORT || 3000;
 // Start the server
 if (process.env.NODE_ENV === "production") {
   // Use Webhooks for the production server
-  const app = express();
   app.use(express.json());
   app.use(webhookCallback(bot, "express"));
 
   app.listen(PORT, () => {
-    console.log(`Bot listening on port ${PORT}`);
+    console.log(`Webligh runing at .... ${PORT}`);
   });
 } else {
   // Use Long Polling for development
   bot.start();
   app.listen(PORT, () => {
-    console.log(`Bot listening on port ${PORT}`);
+    console.log(`Webligh runing at ....  ${PORT}`);
   });
 }
