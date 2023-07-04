df6d7d2b35b37cc7cf53229935ac48ac30dc1413
weblight <weblight@localhost.localdomain>
2023-07-04 21:00:48 +0600

    fix serve2

--- index.js
+++ index.js
@@ -14,16 +14,16 @@ if (process.env.NODE_ENV === "production") {
   // Use Webhooks for the production server
   app.use(express.json());
   app.use(webhookCallback(bot, "express"));
-  
+
   bot.start();
 
   app.listen(PORT, () => {
-    console.log(`Webligh runing at .... ${PORT}`);
+    console.log(`Webligh runing at .... ${PORT} on ${process.env.NODE_ENV}` );
   });
 } else {
   // Use Long Polling for development
   bot.start();
   app.listen(PORT, () => {
-    console.log(`Webligh runing at ....  ${PORT}`);
+    console.log(`Webligh runing at .... ${PORT} on ${process.env.NODE_ENV}` );
   });
 }
