890dc1585028bd304cc145d5a76ddaa5705c56be
weblight <weblight@localhost.localdomain>
2023-07-04 20:18:40 +0600

    init deply for telegram bot to cyclick

--- index.js
+++ index.js
@@ -1,73 +1,29 @@
-const express = require('express')
-const app = express()
-const db = require('@cyclic.sh/dynamodb')
+const express = require('express');
+const bot = require('./src-back/bot');
+const app = require('./src-back/routes');
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
 
-// #############################################################################
-// This configures static hosting for files in /public that have the extensions
-// listed in the array.
-var options = {
-  dotfiles: 'ignore',
-  etag: false,
-  extensions: ['htm', 'html','css','js','ico','jpg','jpeg','png','svg'],
-  index: ['index.html'],
-  maxAge: '1m',
-  redirect: false
-}
-app.use(express.static('public', options))
-// #############################################################################
 
-// Create or Update an item
+const PORT = process.env.PORT || 3000;
 
-app.post('/:col/:key', async (req, res) => {
-  console.log(req.body)
+// Start the bot server
+// bot.start();
 
-  const col = req.params.col
-  const key = req.params.key
-  console.log(`from collection: ${col} delete key: ${key} with params ${JSON.stringify(req.params)}`)
-  const item = await db.collection(col).set(key, req.body)
-  console.log(JSON.stringify(item, null, 2))
-  res.json(item).end()
-})
-
-// Delete an item
-app.delete('/:col/:key', async (req, res) => {
-  const col = req.params.col
-  const key = req.params.key
-  console.log(`from collection: ${col} delete key: ${key} with params ${JSON.stringify(req.params)}`)
-  const item = await db.collection(col).delete(key)
-  console.log(JSON.stringify(item, null, 2))
-  res.json(item).end()
-})
-
-// Get a single item
-app.get('/:col/:key', async (req, res) => {
-  const col = req.params.col
-  const key = req.params.key
-  console.log(`from collection: ${col} get key: ${key} with params ${JSON.stringify(req.params)}`)
-  const item = await db.collection(col).get(key)
-  console.log(JSON.stringify(item, null, 2))
-  res.json(item).end()
-})
-
-// Get a full listing
-app.get('/:col', async (req, res) => {
-  const col = req.params.col
-  console.log(`list collection: ${col} with params: ${JSON.stringify(req.params)}`)
-  const items = await db.collection(col).list()
-  console.log(JSON.stringify(items, null, 2))
-  res.json(items).end()
-})
-
-// Catch all handler for all other request.
-app.use('*', (req, res) => {
-  res.json({ msg: 'no route handler found' }).end()
-})
 
 // Start the server
-const port = process.env.PORT || 3000
-app.listen(port, () => {
-  console.log(`index.js listening on ${port}`)
-})
+if (process.env.NODE_ENV === "production") {
+  // Use Webhooks for the production server
+  const app = express();
+  app.use(express.json());
+  app.use(webhookCallback(bot, "express"));
+
+  app.listen(PORT, () => {
+    console.log(`Bot listening on port ${PORT}`);
+  });
+} else {
+  // Use Long Polling for development
+  bot.start();
+  app.listen(PORT, () => {
+    console.log(`Bot listening on port ${PORT}`);
+  });
+}
---
+++ src-back/bot.js
@@ -0,0 +1,153 @@
+const { Bot, InlineKeyboard, webhookCallback } = require("grammy");
+const { chunk } = require("lodash");
+const { applyTextEffect } = require("./textEffects");
+
+// Create a bot using the Telegram token
+const bot = new Bot(process.env.TELEGRAM_TOKEN || "");
+
+// Handle the /yo command to greet the user
+bot.command("yo", (ctx) => ctx.reply(`Yo ${ctx.from.username}`));
+
+// Handle the /effect command to apply text effects using an inline keyboard
+const allEffects = [
+  { code: "w", label: "Monospace" },
+  { code: "b", label: "Bold" },
+  { code: "i", label: "Italic" },
+  { code: "d", label: "Doublestruck" },
+  { code: "o", label: "Circled" },
+  { code: "q", label: "Squared" },
+];
+
+const effectCallbackCodeAccessor = (effectCode) => `effect-${effectCode}`;
+
+const effectsKeyboardAccessor = (effectCodes) => {
+  const effectsAccessor = (effectCodes) =>
+    effectCodes.map((code) =>
+      allEffects.find((effect) => effect.code === code)
+    );
+  const effects = effectsAccessor(effectCodes);
+
+  const keyboard = new InlineKeyboard();
+  const chunkedEffects = chunk(effects, 3);
+  for (const effectsChunk of chunkedEffects) {
+    for (const effect of effectsChunk) {
+      effect &&
+        keyboard.text(effect.label, effectCallbackCodeAccessor(effect.code));
+    }
+    keyboard.row();
+  }
+
+  return keyboard;
+};
+
+const textEffectResponseAccessor = (originalText, modifiedText) =>
+  `Original: ${originalText}` +
+  (modifiedText ? `\nModified: ${modifiedText}` : "");
+
+const parseTextEffectResponse = (response) => {
+  const originalText = response.match(/Original: (.*)/)[1];
+  const modifiedTextMatch = response.match(/Modified: (.*)/);
+
+  let modifiedText;
+  if (modifiedTextMatch) modifiedText = modifiedTextMatch[1];
+
+  if (!modifiedTextMatch) return { originalText };
+  else return { originalText, modifiedText };
+};
+
+bot.command("effect", (ctx) =>
+  ctx.reply(textEffectResponseAccessor(ctx.match), {
+    reply_markup: effectsKeyboardAccessor(
+      allEffects.map((effect) => effect.code)
+    ),
+  })
+);
+
+// Handle inline queries
+const queryRegEx = /effect (monospace|bold|italic) (.*)/;
+bot.inlineQuery(queryRegEx, async (ctx) => {
+  const fullQuery = ctx.inlineQuery.query;
+  const fullQueryMatch = fullQuery.match(queryRegEx);
+  if (!fullQueryMatch) return;
+
+  const effectLabel = fullQueryMatch[1];
+  const originalText = fullQueryMatch[2];
+
+  const effectCode = allEffects.find(
+    (effect) => effect.label.toLowerCase() === effectLabel.toLowerCase()
+  ).code;
+  const modifiedText = applyTextEffect(originalText, effectCode);
+
+  await ctx.answerInlineQuery([
+    {
+      type: "article",
+      id: "text-effect",
+      title: "Text Effects",
+      input_message_content: {
+        message_text: `Original: ${originalText}\nModified: ${modifiedText}`,
+        parse_mode: "HTML",
+      },
+      reply_markup: new InlineKeyboard().switchInline("Share", fullQuery),
+      url: "http://t.me/EludaDevSmarterBot",
+      description: "Create stylish Unicode text, all within Telegram.",
+    },
+  ]);
+});
+
+// Return empty result list for other queries.
+bot.on("inline_query", (ctx) => ctx.answerInlineQuery([]));
+
+// Handle text effects from the effect keyboard
+for (const effect of allEffects) {
+  const allEffectCodes = allEffects.map((effect) => effect.code);
+
+  bot.callbackQuery(effectCallbackCodeAccessor(effect.code), async (ctx) => {
+    const { originalText } = parseTextEffectResponse(ctx.msg.text || "");
+    const modifiedText = applyTextEffect(originalText, effect.code);
+
+    await ctx.editMessageText(
+      textEffectResponseAccessor(originalText, modifiedText),
+      {
+        reply_markup: effectsKeyboardAccessor(
+          allEffectCodes.filter((code) => code !== effect.code)
+        ),
+      }
+    );
+  });
+}
+
+// Handle the /about command
+const aboutUrlKeyboard = new InlineKeyboard().url(
+  "Host your own bot for free.",
+  "https://cyclic.sh/"
+);
+
+// Suggest commands in the menu
+bot.api.setMyCommands([
+  { command: "yo", description: "Be greeted by the bot" },
+  {
+    command: "effect",
+    description: "Apply text effects on the text. (usage: /effect [text])",
+  },
+]);
+
+// Handle all other messages and the /start command
+const introductionMessage = `Hello! I'm a Telegram bot.
+I'm powered by Cyclic, the next-generation serverless computing platform.
+
+<b>Commands</b>
+/yo - Be greeted by me
+/effect [text] - Show a keyboard to apply text effects to [text]`;
+
+const replyWithIntro = (ctx) =>
+  ctx.reply(introductionMessage, {
+    reply_markup: aboutUrlKeyboard,
+    parse_mode: "HTML",
+  });
+
+bot.command("start", replyWithIntro);
+bot.on("message", replyWithIntro);
+
+
+// Export the bot instance
+module.exports = bot;
