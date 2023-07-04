17df303f18a5f5b12a915c422d9dc0a35d9d321a
Faisal Md <faisalindex@gmail.com>
2023-07-04 23:16:19 +0600

    addHash3

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const app = require('./src-back/routes');
 const bot = require('./src-back/bot');
 const fs = require("fs");
+const path = require("path");
+
 
 
 const PORT = process.env.PORT || 3000;
@@ -9,10 +11,10 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, async () => {
   let hash;
   try {
-    hash = await fs.promises.readFile(".git/refs/heads/master");
+    hash = await fs.promises.readFile(path.join(__dirname, ".git/refs/heads/master"));
     console.log(hash.toString().trim());
   } catch (error) {
     console.error("Error reading file:", error);
   }
-  console.log(`App running: | Port: ${PORT} | Env: ${process.env.NODE_ENV} | Hash when start: ${hash.slice(0, 6)} ` );
+  console.log(`App running: | Port: ${PORT} | Env: ${process.env.NODE_ENV} | Hash when start: ${hash} ` );
 });
