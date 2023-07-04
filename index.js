645e9641ae2cd42661ddaae203b168b7b78bd5ca
weblight <weblight@localhost.localdomain>
2023-07-04 23:16:11 +0600

    working Bot @ production

--- index.js
+++ index.js
@@ -7,25 +7,9 @@ const bot = require('./src-back/bot');
 const PORT = process.env.PORT || 3000;
 
 
-// // Start the server
-// if (process.env.NODE_ENV === "production") {
-//   // Use Webhooks for the production server
-//   app.use(express.json());
-//   app.use(webhookCallback(bot, "express"));
 
-
-//   app.listen(PORT, () => {
-//     console.log(`|3|Webligh runing at .... ${PORT} on ${process.env.NODE_ENV}` );
-//   });
-// } else {
-//   // Use Long Polling for development
-//   bot.start();
-//   app.listen(PORT, () => {
-//     console.log(`||Webligh runing at .... ${PORT} on ${process.env.NODE_ENV}` );
-//   });
-// }
 
 
 app.listen(PORT, () => {
-  console.log(`|yess|Webligh runing at .... ${PORT} on ${process.env.NODE_ENV}` );
+  console.log(`| Bot work |Webligh runing at .... ${PORT} on ${process.env.NODE_ENV}` );
 });
